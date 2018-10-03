500debc665f79fd8bcc6f18ed60f9aa88cdf7a45
Donizety <dbaltokoski@gmail.com>
2018-10-03 14:03:45 -0300

    recover user data on page load

--- src/App.vue
+++ src/App.vue
@@ -22,14 +22,35 @@
 
 <script>
 import { mapActions } from 'vuex'
+import router from '@/router'
+const axios = require('axios')
 
 export default {
   name: 'App',
   methods: {
-    ...mapActions(['unloadUser']),
+    ...mapActions(['unloadUser', 'loadUser']),
     logOut () {
       this.unloadUser()
       this.$cookie.delete('SecureToken')
+    },
+    doLogin (response) {
+      this.loadUser(response.data)
+      if (this.$route.name === 'sign') {
+        router.push('/')
+      }
+    }
+  },
+  created () {
+    let token = this.$cookie.get('SecureToken')
+    if (token) {
+      axios
+        .get('https://sabre-api.herokuapp.com/api/v1/user', {
+          headers: { 'X-Secure-Token': token }
+        })
+        .then(this.doLogin)
+        .catch(this.logOut)
+    } else {
+      console.log('no secure token')
     }
   }
 }
