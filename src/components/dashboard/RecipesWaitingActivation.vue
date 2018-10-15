2256766477ed2e3d89752e0d1cdedc4e9a377890
Donizety <dbaltokoski@gmail.com>
2018-10-15 19:24:24 -0300

    add recipe card

--- src/components/dashboard/RecipesWaitingActivation.vue
+++ src/components/dashboard/RecipesWaitingActivation.vue
@@ -1,22 +1,39 @@
 <template>
   <dashboard-frame :title="title">
     <div>
-      Recipes Waiting Activation Placeholder
+      <recipe-card v-for="recipe in recipes" :key="'recipe' + recipe.id" :recipe="recipe"></recipe-card>
     </div>
   </dashboard-frame>
 </template>
 
 <script>
 import Frame from './Frame.vue'
+import RecipeCard from '@/components/shared/RecipeCard.vue'
+const axios = require('axios')
 
 export default {
   data () {
     return {
-      title: 'Receitas Para Liberar'
+      title: 'Receitas Para Liberar',
+      loading: true,
+      recipes: []
     }
   },
   components: {
-    dashboardFrame: Frame
+    dashboardFrame: Frame,
+    recipeCard: RecipeCard
+  },
+  created () {
+    let token = this.$cookie.get('SecureToken')
+    axios
+      .get('https://sabre-api.herokuapp.com/api/v1/users/recipe?status=waiting_activation', {
+        headers: { 'X-Secure-Token': token }
+      })
+      .then((response) => {
+        this.recipes = response.data
+        this.loading = false
+      })
+      .catch((error) => { console.log(error) })
   }
 }
 </script>
---
+++ src/components/shared/RecipeCard.vue
@@ -0,0 +1,53 @@
+<template>
+    <md-card>
+        <md-card-media>
+            <img src="/static/img/demo.jpg" alt="Demo">
+        </md-card-media>
+        <md-card-header>
+            <div class="md-title">{{ recipe.name }}</div>
+            <div class="md-subhead">{{ recipe.owner }}</div>
+        </md-card-header>
+
+        <md-card-expand>
+            <md-card-actions md-alignment="space-between">
+                <div>
+                    <md-button>Receita</md-button>
+                </div>
+
+                <md-card-expand-trigger @click="showRecipe = !showRecipe">
+                    <md-button>Ingredientes</md-button>
+                </md-card-expand-trigger>
+            </md-card-actions>
+
+            <md-card-expand-content>
+                <md-card-content v-html="ingredients">
+                </md-card-content>
+            </md-card-expand-content>
+
+        </md-card-expand>
+    </md-card>
+</template>
+
+<script>
+export default {
+  props: [ 'recipe' ],
+  computed: {
+    ingredients () {
+      let htmlItens = ''
+      this.recipe.ingredients.split('\n').forEach(element => {
+        htmlItens += `<p>${element}</p>`
+      })
+      return htmlItens
+    }
+  }
+}
+</script>
+
+<style scoped>
+.md-card {
+    width: 320px;
+    margin: 4px;
+    display: inline-block;
+    vertical-align: top;
+}
+</style>
