2ef0d206fae372a2c09949feb3ef1f1b7638e23e
Donizety <dbaltokoski@gmail.com>
2019-04-01 19:42:21 -0300

    deploy with gcloud

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,5 @@ docker build -t donibtk/beta-sabre-web:latest -t donibtk/beta-sabre-web:$SHA ./
 docker push donibtk/beta-sabre-web:latest
 
 docker push donibtk/beta-sabre-web:$SHA
+
+kubectl set image deployments/client-deployment client=donibtk/beta-sabre-web:$SHA
